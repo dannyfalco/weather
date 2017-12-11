dd7ac5f32812e70bbaa1e55ee9cc1d49d3faeb39
Falco Danny (CORP) <danny.falco@adp.com>
2017-12-10 21:33:23 -0500

    node weather app

---
+++ index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'fe1e58ee9ca44bedeb476c0a86836ab4';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body);
+    let message = `It’s ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
